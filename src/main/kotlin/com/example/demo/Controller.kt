b358e87aa5929e20c2cb8b98eb0d06321d8bf593
CosMark1 <marber248@alu.centroafuera.es>
2022-03-14 18:08:58 +0100

    a

--- src/main/kotlin/com/example/demo/Controller.kt
+++ src/main/kotlin/com/example/demo/Controller.kt
@@ -4,6 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.security.MessageDigest
+import java.util.*
+import javax.crypto.BadPaddingException
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
 
 @RestController
 class Controller(
@@ -74,12 +79,52 @@ class Controller(
         return Error(3, "Pass de administrador incorrecta")
     }
 
+    @GetMapping("obtenerMensajesDescifrados")
+    fun obtenerMensajesDescifrados(@RequestBody admin: Admin): Any {
 
-
-
-
-
-        fun vaciarLista() {
-            Lista.list.clear()
+        adminsRepository.findAll().forEach { admin1 ->
+            if (admin1.Nombre == admin.Nombre) {
+                if (admin1.Pass == admin.Pass) {
+                    mensajesRepository.findAll().forEach { mensaje ->
+                        val textoDescifrado: String = try {
+                            descifrar(mensaje.texto, usuariosRepository.getById(mensaje.usuarioId).cifrado)
+                        } catch (e: Exception) {
+                            return "Texto indescifrable"
+                        }
+                        mensaje.texto = textoDescifrado
+                        Lista.list.add(mensaje)
+                    }
+                    return listOf(Lista)
+                }
+            }
         }
+        return listOf(Error(3, "Pass de administrador incorrecta"))
     }
+
+
+    @Throws(BadPaddingException::class)
+    private fun descifrar(textoCifradoYEncodado: String, llaveEnString: String): String {
+        val type = "AES/ECB/PKCS5Padding"
+        println("Voy a descifrar $textoCifradoYEncodado")
+        val cipher = Cipher.getInstance(type)
+        cipher.init(Cipher.DECRYPT_MODE, getKey(llaveEnString))
+        val textCifradoYDencodado = Base64.getUrlDecoder().decode(textoCifradoYEncodado)
+        println("Texto cifrado $textCifradoYDencodado")
+        val textDescifradoYDesencodado = String(cipher.doFinal(textCifradoYDencodado))
+        println("Texto cifrado y desencodado $textDescifradoYDesencodado")
+        return textDescifradoYDesencodado
+    }
+
+    private fun getKey(llaveEnString: String): SecretKeySpec {
+        var llaveUtf8 = llaveEnString.toByteArray(Charsets.UTF_8)
+        val sha = MessageDigest.getInstance("SHA-1")
+        llaveUtf8 = sha.digest(llaveUtf8)
+        llaveUtf8 = llaveUtf8.copyOf(16)
+        return SecretKeySpec(llaveUtf8, "AES")
+    }
+
+
+    fun vaciarLista() {
+        Lista.list.clear()
+    }
+}
